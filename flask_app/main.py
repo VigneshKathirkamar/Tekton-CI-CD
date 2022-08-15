bbd6bab974baed017d11afa452aca3cbb0eda60c
VigneshKathirkamar <i51vignesh@gmail.com>
2022-08-15 12:35:34 +0530

    Initial commit

---
+++ flask_app/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, How are you buddy?"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
+    # "0.0.0.0", tells a server to "listen" for and accept connections from any IP address. 
